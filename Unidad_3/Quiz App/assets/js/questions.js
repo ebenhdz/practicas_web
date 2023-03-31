c358bdb1ed34d39bbbd21018f8f2e81f289533b8
Edgar Benito Hernandez <ebenhdz@Ordenador-portatil-de-Edgar.local>
2023-03-31 10:05:46 -0600

    Se agrega archivo de questions

---
+++ Unidad_3/Quiz App/assets/js/questions.js
@@ -0,0 +1,49 @@
+const PREGUNTAS = [{
+    "name": "¿Cual es el rio mas largo del planeta?",
+    "answers": [
+        {
+            "answer": "Amazonas",
+            "isRight": true
+        },
+        {
+            "answer": "Mississipi",
+            "isRight": false
+        },
+        {
+            "answer": "Nilo",
+            "isRight": false
+        }
+    ]
+}, {
+    "name": "¿Cual es la persona mas seguida en Instagram?",
+    "answers": [
+        {
+            "answer": "Elon Musk",
+            "isRight": false
+        },
+        {
+            "answer": "Cristiano Ronaldo",
+            "isRight": true
+        },
+        {
+            "answer": "Donal Trump",
+            "isRight": false
+        }
+    ]
+}, {
+    "name": "¿En que año fueron los juegos olimpicos de Londres?",
+    "answers": [
+        {
+            "answer": "2012",
+            "isRight": true
+        },
+        {
+            "answer": "2020",
+            "isRight": false
+        },
+        {
+            "answer": "1994",
+            "isRight": false
+        }
+    ]
+}];
