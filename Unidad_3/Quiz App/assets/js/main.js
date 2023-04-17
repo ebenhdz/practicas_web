aebf34ebde1879f4b42e523a7ae7be5d96fa3a93
Edgar Benito Hernandez <ebenhdz@Ordenador-portatil-de-Edgar.local>
2023-04-17 08:50:36 -0600

    Quiz Has terminado

--- Unidad_3/Quiz App/assets/js/main.js
+++ Unidad_3/Quiz App/assets/js/main.js
@@ -1,3 +1,5 @@
+import runConfetti from './confetti.js';
+
 var formInicio = document.getElementById("form-start");
 
 var seccionInicio = document.getElementById("home");
@@ -9,7 +11,9 @@ var indicePreguntaActual = 0;
 formInicio.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var nombre = document.getElementById("name").value;
+    //var nombre = document.getElementById("name").value;
+
+    var nombre = event.target.name.value;
 
     if (nombre.trim() == "") {
         alert("Ingresa tu nombre");
@@ -36,9 +40,48 @@ function cargarPregunta() {
 
     textoPreguntas.innerHTML = preguntaActual.name;
 
-    let template = preguntaActual.answers.map(function(answer) {
-        return `<button class="btn">${answer.answer}</button>`;
+    let template = preguntaActual.answers.map(function(answer, index) {
+        return `
+        <button 
+            class="btn btn-answer"
+            data-index=${index}
+        >
+        ${answer.answer}
+        </button>`;
     }).join("");
 
     divRespuestas.innerHTML = template;
+
+    agregarEventoClickRespuestas();
+}
+
+function agregarEventoClickRespuestas() {
+    let botones = document.querySelectorAll(".btn-answer");
+
+    botones.forEach(function(boton, index) {
+        boton.addEventListener("click", onAnswerClick);
+    })
+}
+
+function onAnswerClick(event) {
+    let numRespuesta = event.target.dataset.index;
+    let preguntaActual = PREGUNTAS[indicePreguntaActual];
+
+    let respuesta = preguntaActual.answers[numRespuesta];
+
+    validarRespuesta(respuesta);
+}
+
+function validarRespuesta(respuesta) {
+    if(respuesta.isRight) alert("Acertaste")
+    else alert("Respuesta Incorrecta");
+
+    indicePreguntaActual++;
+
+    if(indicePreguntaActual == PREGUNTAS.length) {
+        runConfetti();
+        alert("Has terminado con el Quizz");
+        return;
+    }
+    cargarPregunta();
 }
