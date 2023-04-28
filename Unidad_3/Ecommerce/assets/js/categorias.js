edf00a4bc5e562af8baad910d6344c69fd5bd670
Edgar Benito Hernandez <ebenhdz@Ordenador-portatil-de-Edgar.local>
2023-04-28 07:15:28 -0600

    onload method on xmlhttprequest

--- Unidad_3/Ecommerce/assets/js/categorias.js
+++ Unidad_3/Ecommerce/assets/js/categorias.js
@@ -2,13 +2,37 @@
 export default function getCategorias() {
     var xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-           console.log(xhttp.responseText);
+    xhttp.onload = function() {
+        if (this.status == 200) { // Operacion correcta
+            var jsonResponse = JSON.parse(xhttp.responseText);
+           crearCategorias(jsonResponse);
         }
+    }
+    
+    xhttp.onerror = function() {
+        console.log("Ocurrio un error")
     };
 
     xhttp.open("GET", "https://dummyjson.com/products/categories", true);
 
     xhttp.send();
+}
+
+function crearCategorias(lista) {
+    var $fragment = document.createDocumentFragment();
+
+    var $lista = document.getElementById("menu-categorias");
+
+    for(const categoria of lista) {
+        var $li = document.createElement("li");
+        $li.classList.add('main-nav__submenu-item');
+
+        var $link = document.createElement("a");
+        $link.textContent = categoria;
+
+        $li.appendChild($link);
+        $fragment.appendChild($li);
+    }
+
+    $lista.appendChild($fragment);
 }
