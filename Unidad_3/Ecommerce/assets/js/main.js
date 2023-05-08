aa27aa5ab683c97fe99cb3e319652f720a3e9214
Edgar Benito Hernandez <ebenhdz@Ordenador-portatil-de-Edgar.local>
2023-05-08 07:06:33 -0600

    Nuevo diseño cards productos

--- Unidad_3/Ecommerce/assets/js/main.js
+++ Unidad_3/Ecommerce/assets/js/main.js
@@ -1,7 +1,14 @@
-import getCategorias from './categorias.js';
-import getTotalProductosCarrito from './carrito.js';
+import getCategorias from "./categorias.js";
+import getTotalProductosCarrito from "./carrito.js";
+import { buscarProducto } from "./productos.js";
 
-window.addEventListener('DOMContentLoaded', function(){
-    getCategorias();
-    getTotalProductosCarrito();
+window.addEventListener("DOMContentLoaded", function () {
+  getCategorias();
+  getTotalProductosCarrito();
+  agregarListeners();
 });
+
+function agregarListeners() {
+  const $formSearch = document.getElementById("main-search");
+  $formSearch.addEventListener("submit", buscarProducto);
+}
