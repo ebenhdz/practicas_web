d5cb3e22a803a262a95c526f2aaf19103b5ac064
Edgar Benito Hernandez <ebenhdz@Ordenador-portatil-de-Edgar.local>
2023-05-10 07:06:30 -0600

    Se agrega la lib navigo para routing

--- Unidad_3/Ecommerce/assets/js/categorias.js
+++ Unidad_3/Ecommerce/assets/js/categorias.js
@@ -1,38 +1,89 @@
-
 export default function getCategorias() {
-    var xhttp = new XMLHttpRequest();
+  var xhttp = new XMLHttpRequest();
 
-    xhttp.onload = function() {
-        if (this.status == 200) { // Operacion correcta
-            var jsonResponse = JSON.parse(xhttp.responseText);
-           crearCategorias(jsonResponse);
-        }
+  xhttp.onload = function () {
+    if (this.status == 200) {
+      // Operacion correcta
+      var jsonResponse = JSON.parse(xhttp.responseText);
+      crearCategorias(jsonResponse);
     }
-    
-    xhttp.onerror = function() {
-        console.log("Ocurrio un error")
-    };
+  };
 
-    xhttp.open("GET", "https://dummyjson.com/products/categories", true);
+  xhttp.onerror = function () {
+    console.log("Ocurrio un error");
+  };
 
-    xhttp.send();
+  xhttp.open("GET", "https://dummyjson.com/products/categories", true);
+
+  xhttp.send();
 }
 
 function crearCategorias(lista) {
-    var $fragment = document.createDocumentFragment();
+  var $fragment = document.createDocumentFragment();
 
-    var $lista = document.getElementById("menu-categorias");
+  var $lista = document.getElementById("menu-categorias");
 
-    for(const categoria of lista) {
-        var $li = document.createElement("li");
-        $li.classList.add('main-nav__submenu-item');
+  for (const categoria of lista) {
+    var $li = document.createElement("li");
+    $li.classList.add("main-nav__submenu-item");
 
-        var $link = document.createElement("a");
-        $link.textContent = categoria;
+    var $link = document.createElement("a");
+    $link.textContent = categoria;
+    $link.setAttribute("href", "/categoria/" + categoria)
+    $link.setAttribute("data-navigo", "")
 
-        $li.appendChild($link);
-        $fragment.appendChild($li);
-    }
+    /*$link.addEventListener('click', function (event) {
+      route(event)
+    })}*/
 
-    $lista.appendChild($fragment);
+    $li.appendChild($link);
+    $fragment.appendChild($li);
+  }
+
+  $lista.appendChild($fragment);
+}
+
+export async function renderCategoria(categoria) {
+  let $productos = await fetch("https://dummyjson.com/products/category/" + categoria)
+    .then((res) => res.json())
+    .then((res) => templateProductos(res.products));
+
+  const $sectionProductos = document.createElement('section');
+  $sectionProductos.classList.add('productos');
+
+  $sectionProductos.appendChild($productos);
+
+  return $sectionProductos;
+}
+
+function templateProductos(productos) {
+  const fragment = document.createDocumentFragment();
+  const $template = document.querySelector("#template-producto");
+
+  for (const producto of productos) {
+    const $clonArticulo = $template.content.cloneNode(true);
+
+    $clonArticulo.querySelector("img").src = producto.thumbnail;
+
+    $clonArticulo.querySelector(".producto-nombre").textContent =
+      producto.title;
+
+    $clonArticulo.querySelector(".producto-descripcion").textContent =
+      producto.description;
+
+    $clonArticulo.querySelector(".producto-precio").textContent =
+      "$ " + producto.price;
+
+    $clonArticulo
+      .querySelector(".btn-add-cart")
+      .addEventListener("click", agregarProductoCarrito, producto);
+
+    fragment.appendChild($clonArticulo);
+  }
+
+  return fragment;
+}
+
+function agregarProductoCarrito(producto) {
+  console.log("Agregado al carrito")
 }
